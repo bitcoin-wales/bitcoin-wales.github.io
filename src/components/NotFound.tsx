a87b5acc2fb14b582bac3658e7658653959ce9a2
Daniel Morritt <Morritt@users.noreply.github.com>
2025-05-22 22:23:46 +0100

    Add NotFound component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] p-4">
+      <h1 className="text-4xl font-bold mb-4">404</h1>
+      <p className="text-xl">Page not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
